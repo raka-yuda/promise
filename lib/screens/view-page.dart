1b8c28e5fc9daa0efd8aa97bf54a457676f9e312
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-04-26 23:03:22 +0700

    add page view widget

--- lib/screens/view-page.dart
+++ lib/screens/view-page.dart
@@ -2,18 +2,45 @@ import 'package:flutter/material.dart';
 
 class ViewScreen extends StatelessWidget {
   static const String id = 'view-screen';
+
+  PageController _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        height: size.height,
-        width: size.width,
-        child: Center(
-          child: Text(
-            'This is a home screen',
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              width: size.width,
+              child: PageView(
+                controller: _pageController,
+                children: <Widget>[
+                  Container(
+                    color: Colors.pink,
+                  ),
+                  Container(
+                    color: Colors.cyan,
+                  ),
+                  Container(
+                    color: Colors.deepPurple,
+                  ),
+                ],
+              ),
+            ),
           ),
-        ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              width: size.width,
+              child: Center(
+                child: Text(_pageController.page.toString()),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
